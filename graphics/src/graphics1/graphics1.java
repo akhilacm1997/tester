3a2686dd610602f25cdc547308232b7333a70d57
User <User@DDU-44>
2019-09-20 18:43:04 +0530

    graphics

---
+++ graphics/src/graphics1/graphics1.java
@@ -0,0 +1,19 @@
+package graphics1;
+
+public class graphics1 {
+
+	public static void main(String[] args)
+	{
+		int num =19;
+		for(int i=0;i<num;i++)
+		{
+			System.out.println("  ");
+			for(int j=0;j<num;j++)
+			{
+				System.out.print(" *");
+			}
+			
+		}
+	}
+
+}
